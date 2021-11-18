1825d17c3cd9908df777df79b8b166bd5f241c89
Amol_Aher <amolaher92@gmail.com>
2021-11-18 12:10:23 +0530

    Updated router setup

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1 class="text-6xl font-bold text-center p-4 bg-gradient-to-r from-red-300 to-red-600">{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the

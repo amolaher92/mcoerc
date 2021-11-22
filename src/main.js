72eb27badec49bec79e4451ceb99d97a65767480
Amol_Aher <amolaher92@gmail.com>
2021-11-22 12:08:04 +0530

    Add routes

--- src/main.js
+++ src/main.js
@@ -3,5 +3,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/tailwind.css'
 import router from './router'
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
-createApp(App).use(router).mount('#app')
+//createApp(App).use(router,).mount('#app')
+const app = createApp(App);
+app.use(VueSweetalert2)
+app.use(router)
+app.mount('#app')
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <button @click="showAlert">Hello world</button>
 </template>
+
+<script>
+export default {
+  methods: {
+    showAlert() {
+      // Use sweetalert2
+      this.$swal('Hello Vue world!!!');
+    },
+  },
+}
+</script>
